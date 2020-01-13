a1b106c136b83faf6a1cc276be1b82b2d4c03352
mzmt <mrcrius1355@gmail.com>
2020-01-13 19:46:22 +0900

    add fn

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
 fn main() {
-  let x = 5;
-  println!("the value of x is : {}", x);
+    let x = 5;
+    println!("the value of x is : {}", x);
+
+    print_number(5);
+}
+
+
+fn print_number(x: i32) {
+    println!("x is: {}", x);
 }
