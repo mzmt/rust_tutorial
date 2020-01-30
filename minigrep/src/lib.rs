82ffb096cc386c4a830ab4bea022a067e78f5525
mzmt <mrcrius1355@gmail.com>
2020-01-30 17:49:48 +0900

    add empty method and test

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -27,3 +27,24 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     f.read_to_string(&mut contents)?;
     Ok(())
 }
+
+// クエリではなくcontentの方を返すから、contentsと戻り値のライフタイムを同じにする
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
