9e5627226a46f0d86634c6dff3bd92d1ac7fb308
mzmt <mrcrius1355@gmail.com>
2020-01-30 12:34:24 +0900

    make all modules public

--- communicator/src/network/server.rs
+++ communicator/src/network/server.rs
@@ -1,3 +1,2 @@
-
-fn connect() {
+pub fn connect() {
 }
--- communicator/target/debug/deps/communicator-93f3d2015413e393.d
+++ communicator/target/debug/deps/communicator-93f3d2015413e393.d
@@ -1,10 +1,10 @@
-/Users/ryohei/Desktop/rust_tutorial/communicator/target/debug/deps/communicator-93f3d2015413e393.rmeta: src/lib.rs src/network/mod.rs src/network/server.rs src/client.rs
+/Users/ryohei/Desktop/rust_tutorial/communicator/target/debug/deps/communicator-93f3d2015413e393.rmeta: src/lib.rs src/client.rs src/network/mod.rs src/network/server.rs
 
-/Users/ryohei/Desktop/rust_tutorial/communicator/target/debug/deps/libcommunicator-93f3d2015413e393.rlib: src/lib.rs src/network/mod.rs src/network/server.rs src/client.rs
+/Users/ryohei/Desktop/rust_tutorial/communicator/target/debug/deps/libcommunicator-93f3d2015413e393.rlib: src/lib.rs src/client.rs src/network/mod.rs src/network/server.rs
 
-/Users/ryohei/Desktop/rust_tutorial/communicator/target/debug/deps/communicator-93f3d2015413e393.d: src/lib.rs src/network/mod.rs src/network/server.rs src/client.rs
+/Users/ryohei/Desktop/rust_tutorial/communicator/target/debug/deps/communicator-93f3d2015413e393.d: src/lib.rs src/client.rs src/network/mod.rs src/network/server.rs
 
 src/lib.rs:
+src/client.rs:
 src/network/mod.rs:
 src/network/server.rs:
-src/client.rs:
